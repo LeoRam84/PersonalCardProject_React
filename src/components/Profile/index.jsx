83298e259097fdc93fe3c50ac0a207b15cd32bdd
Leonardo Ramidan <leonardoramidan@hotmail.com>
2023-09-05 21:01:15 -0300

    Segunda Versão GitHub

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,18 +1,44 @@
+import { useState } from "react";
+import LinkButton from "../LinkButton";
+import ProfileSection from "../ProfileSection";
+import Title from "../Title";
 import styles from "./styles.module.css";
 
+
 export default function Profile(props) {
+  // [valor, funçãoModificadora]
+  const [followText, setFollowText] = useState("Follow")
+
+  function handleClick(ev) {
+    alert("Você agora está me seguindo!")
+    setFollowText("Following")
+  }
+    
   return (
     <div className={styles.container}>
       <img className={styles.avatar} src={props.avatar} alt={props.name} />
-      <h2>{props.name}</h2>
-      <div>{props.bio}</div>
-      <div>{props.phone}</div>
-      <div>{props.email}</div>
-      <div className={styles.links}>
-        <a href={props.githubUrl} target="_blank">GitHub</a>
-        <a href={props.linkedinUrl} target="_blank">LinkedIn</a>
-        <a href={props.twitterUrl} target="_blank">Twitter</a>
-      </div>
+      <Title>
+        <span>{props.name}</span>
+        <button
+          onClick={handleClick}
+          className={styles.followBtn}
+        >
+          {followText}
+        </button>
+      </Title>
+      <ProfileSection>{props.bio}</ProfileSection>
+      <ProfileSection>{props.phone}</ProfileSection>
+      <ProfileSection>{props.email}</ProfileSection>
+      <ProfileSection 
+        className={styles.links}
+        id="links-section"
+        data-test="some value"
+        aria-label="social links"
+      >
+        <LinkButton href={props.githubUrl}>GitHub</LinkButton>
+        <LinkButton href={props.linkedinUrl}>LinkedIn</LinkButton>
+        <LinkButton href={props.twitterUrl}>Twitter</LinkButton>
+      </ProfileSection>
     </div>
   )
 }
